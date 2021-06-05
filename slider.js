9113469589cbceb530fad158cf6e62bc15acfd09
Praharsh Singh <praharsh535@gmail.com>
2021-06-05 09:17:05 +0530

    first commit

---
+++ slider.js
@@ -0,0 +1,33 @@
+$(".row").slick({
+  arrows: false,
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  rows: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
